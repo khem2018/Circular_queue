d730c1d610e36a79695021330dde98766c3d63f5
Khem Sharma <43295209+khem2018@users.noreply.github.com>
2020-03-22 16:43:09 +0530

    Add files via upload

---
+++ Circular_queue.py
@@ -0,0 +1,118 @@
+
+# coding: utf-8
+
+# In[120]:
+
+
+class Circular_queue:  
+    class _Node:
+        def __init__(self, data, pre=None, nxt=None):
+            self._data = data
+            self._pre= pre
+            self._nxt = nxt     
+    
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._size = 0  
+    
+    def enqueue(self, data):
+        new_node = self._Node(data=data)
+        if not self._head:
+            self._head = new_node
+            self._tail = new_node
+        else:
+            temp = self._head
+            self._head = new_node
+            self._head._nxt= temp
+            temp._pre=self._head
+            self._tail._nxt = self._head  
+        self._size +=1
+        print('Element inserted: {0}'.format(self._head._data))
+
+    def dequeue_from_front(self):
+        rep = self._head
+        self._head = self._head._nxt
+        self._head._pre = None
+        self._size -=1
+        print('Element removed: {0}'.format(rep._data))
+    
+    def dqueue_from_end(self):
+        rep = self._tail
+        self._tail = self._tail._pre
+        self._tail._nxt = self._head
+        self._size -=1
+        print('Element removed: {0}'.format(rep._data))
+    
+    def get_size(self):
+        return self._size
+    
+    def print_queue(self):
+        initial_pointer = self._head
+        counter = 0
+        while counter <=self._size:
+            print(initial_pointer._data)
+            print('Circular_queue: {}'.format(initial_pointer._data))
+            initial_pointer = initial_pointer._nxt
+            counter+=1
+
+
+# In[121]:
+
+
+obj = Circular_queue()
+
+
+# In[122]:
+
+
+obj.enqueue(2)
+
+
+# In[123]:
+
+
+obj.enqueue(3)
+
+
+# In[124]:
+
+
+obj.enqueue(4)
+
+
+# In[125]:
+
+
+obj.enqueue(5)
+
+
+# In[126]:
+
+
+obj.enqueue(6)
+
+
+# In[127]:
+
+
+obj.get_size()
+
+
+# In[128]:
+
+
+obj.dequeue_from_front()
+
+
+# In[129]:
+
+
+obj.dqueue_from_end()
+
+
+# In[130]:
+
+
+obj.print_queue()
+
